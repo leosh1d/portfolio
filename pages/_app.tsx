6d12345bee701051bbf2e71f00595138cfcb1565
leosh1d <leosh1dddd@gmail.com>
2022-06-21 16:51:08 +0400

    preview + new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,9 @@ import Router from "next/router"
 import nProgress from "nprogress"
 import type { AppProps } from "next/app"
 
+import Head from "next/head"
+import Preview from "../public/preview.jpg"
+
 import { ApolloProvider } from "@apollo/client"
 
 import client from "../src/components/graphql/init"
@@ -44,13 +47,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [router.events])
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider attribute="class">
-        <main className="flex flex-col bg-white dark:bg-black_dark ease-out-quad duration-200">
-          <Component {...pageProps} key={router.route} />
-        </main>
-      </ThemeProvider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <meta property="og:image" content={Preview.src} />
+      </Head>
+      <ApolloProvider client={client}>
+        <ThemeProvider attribute="class">
+          <main className="flex flex-col bg-white dark:bg-black_dark ease-out-quad duration-200">
+            <Component {...pageProps} key={router.route} />
+          </main>
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
   )
 }
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
 import Script from "next/script"
 
+import Preview from "../public/preview.jpg"
+
 class MyDocument extends Document {
   render() {
     return (
@@ -12,6 +14,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Source+Code+Pro:wght@700&display=swap"
             rel="stylesheet"
           />
+          <meta property="og:title" content="leosh1d" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://leosh1d.site" />
           <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X7VME9VL8T" />
           <Script
             dangerouslySetInnerHTML={{
